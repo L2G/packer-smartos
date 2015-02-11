bf322a487256385541456c5a66500fd9d663786e
Larry Gilbert <larry@industrialtoys.com>
2015-02-10 16:59:12 -0800

    Changes to post-install provisioning script

--- post_install.bash
+++ post_install.bash
@@ -5,15 +5,14 @@ zone_image_id=$(imgadm avail | awk '{if ($2 == "base64" && $3 == "14.3.0") print
 imgadm import ${zone_image_id}
 
 # Create a zone
-cat >vagrant-guest.json <<EOF
+cat >/usbkey/vagrant-guest.json <<EOF
 {
   "brand": "joyent",
   "image_uuid": "${zone_image_id}",
   "alias": "vagrant-guest",
   "hostname": "vagrant-guest",
-  "max_physical_memory": 512
+  "autoboot": true
 }
 EOF
 
-vm_id=$(vmadm create -f vagrant-guest.json | awk '/^Successfully created / {print $3}')
-rm vagrant-guest.json
+vmadm create -f /usbkey/vagrant-guest.json && rm /usbkey/vagrant-guest.json

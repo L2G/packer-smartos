b0517ad09e652d25ceabafedf9030a79919eb84b
Larry Gilbert <larry@industrialtoys.com>
2015-02-12 15:12:50 -0800

    Don't set up a vagrant0 interface on the barebones box

--- provision_barebones.bash
+++ provision_barebones.bash
@@ -3,4 +3,3 @@
 # Set up a virtual NIC for use by Vagrant boxes that build upon this one
 eth_mac=$(dladm show-phys -mpo address)
 echo "vagrant_nic=${eth_mac}" >> /usbkey/config
-echo "vagrant0_ip=dhcp" >> /usbkey/config

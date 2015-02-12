84ab01da7b173b804148aadce933d4e18fadc0d2
Larry Gilbert <larry@industrialtoys.com>
2015-02-12 15:13:22 -0800

    Several changes and improvements to the base64 provisioning script
    - Make the shell script die on error in appropriate places
    - Change the VM's hostname from "vagrant-guest" to "vagrant-base64"
    - Refine the SmartOS VM networking
    - Validate the VM JSON data before creating
    - Because `vmadm create` seems to fail the first 4 or 5 times it's
      attempted, try it up to 10 times

--- provision_base64.bash
+++ provision_base64.bash
@@ -1,25 +1,47 @@
 #!/bin/bash
 
+# Make script errors fatal
+set -e
+
 # Install base64 14.3.0 zone image
 zone_image_id=$(imgadm avail | awk '{if ($2 == "base64" && $3 == "14.3.0") print $1}')
 imgadm import ${zone_image_id}
 
 # Create a zone
-cat >/usbkey/vagrant-guest.json <<EOF
+vm_json_file=/usbkey/vagrant-guest.json
+cat >${vm_json_file} <<EOF
 {
   "brand": "joyent",
   "image_uuid": "${zone_image_id}",
-  "alias": "vagrant-guest",
-  "hostname": "vagrant-guest",
+  "alias": "vagrant-base64",
+  "hostname": "vagrant-base64",
   "autoboot": true,
+  "resolvers": ["10.0.2.3"],
+  "maintain_resolvers": true,
   "nics": [
     {
       "nic_tag": "vagrant",
-      "ip": "dhcp",
-      "primary": true
+      "ip": "dhcp"
     }
   ]
 }
 EOF
 
-vmadm create -f /usbkey/vagrant-guest.json && rm /usbkey/vagrant-guest.json
+# Catch any stupid mistakes
+vmadm validate create -f ${vm_json_file}
+
+# Make errors non-fatal again
+set +e
+
+# This almost never works on the first 4 or 5 tries, so we try 10 times
+for attempt in {1..10}; do
+  echo "Creating OS VM (attempt $attempt of 10)..."
+  if vmadm create -f ${vm_json_file}; then break
+  else
+    echo "Fail! Mopping up..."
+    vmadm delete $(vmadm list -po uuid)
+  fi
+done
+
+# Check to make sure the VM got installed and is running
+vmadm list | grep -q running

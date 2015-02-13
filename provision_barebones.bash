509e3a1b2fabd2dcf3583ec61ca9fd4404afdefd
Larry Gilbert <larry@industrialtoys.com>
2015-02-12 18:52:36 -0800

    Fix handling of list of Ethernet MACs in barebones script

--- provision_barebones.bash
+++ provision_barebones.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Set up a virtual NIC for use by Vagrant boxes that build upon this one
-eth_mac=$(dladm show-phys -mpo address)
-echo "vagrant_nic=${eth_mac}" >> /usbkey/config
+eth_mac=($(dladm show-phys -mpo address))
+echo "vagrant_nic=${eth_mac[1]}" >> /usbkey/config
